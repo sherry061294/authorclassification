5e9e9655905e493ff1e34a8f8a3ff45bba5e9240
dctodka <tspaightm@gmail.com>
2020-05-02 01:21:25 -0400

    Adding text processing and generation code. Also moving files into subdirectories.

---
+++ generation/generate_3gram.py
@@ -0,0 +1,82 @@
+import numpy as np
+import os
+
+def generate_sentence(word_probs, max_words):
+    '''
+    Generates a sentence based on word counts
+    '''
+    p2_word = "<s>"
+    p1_word = "<s>"
+    sentence = "<s> "
+
+    for i in range(max_words):
+        word_prob = word_probs[p2_word][p1_word]
+        next_word = np.random.choice(
+            [word for word in word_prob], # list of words to choose from
+            1, # number of words to choose
+            [prob for _, prob in word_prob.items()])[0] # weights
+
+        # stop generation if we've hit the end sentence tag
+        if next_word == "</s>":
+            break
+
+        # add word to sentence and reset previous words
+        sentence += next_word + " "
+        p2_word = p1_word
+        p1_word = next_word
+
+    return sentence + "</s>"
+
+
+if __name__ == "__main__":
+    directory = "%s/../data/processed_data" % os.getcwd()
+
+    # for each author
+    for filename in os.listdir(directory):
+        # initialize dictionary with sentence begin tags
+        word_probs = dict()
+        word_probs["<s>"] = dict()
+        word_probs["<s>"]["<s>"] = dict()
+
+        # read through authors sentences
+        with open(os.path.join(directory, filename), "r") as ifile:
+
+            # iterate through file and update counts
+            for line in ifile:
+                sentence = line[:-1].split(" ") # skip the trailing newline character
+
+                # initialize sentence tags
+                p2_word = "<s>"
+                p1_word = "<s>"
+
+                for word in sentence[1:]:
+                    # initialize empty dictionaries for new words
+                    if p2_word not in word_probs:
+                        word_probs[p2_word] = dict()
+                    if p1_word not in word_probs[p2_word]:
+                        word_probs[p2_word][p1_word] = dict()
+
+                    # if we have not seen this word combination, initialize count to 1
+                    if word not in word_probs[p2_word][p1_word]:
+                        word_probs[p2_word][p1_word][word] = 1
+                    else:
+                        word_probs[p2_word][p1_word][word] += 1 # increment the count of this 3-word run
+
+                    # reset previous words
+                    p2_word = p1_word
+                    p1_word = word
+
+        # normalize counts
+        for p2_word in word_probs:
+            for p1_word in word_probs[p2_word]:
+                tot = sum(word_probs[p2_word][p1_word].values())
+                for word in word_probs[p2_word][p1_word]:
+                  word_probs[p2_word][p1_word][word] = word_probs[p2_word][p1_word][word] / tot
+
+        # generate sentences
+        num_sentences = 100
+        max_length = 30
+        with open("%s/../data/generated_data/3gram/%s" % (os.getcwd(), filename), "w") as ofile:
+            for _ in range(num_sentences):
+                sentence =  generate_sentence(word_probs, max_length)
+                ofile.write(sentence + "\n")
---
+++ text_processing/process_text.py
@@ -0,0 +1,60 @@
+from collections import Counter
+
+import os
+import re
+
+def process_text(line):
+	line = line.replace("\n", " ") # remove newline
+	line = line.replace("Mrs.", "Mrs") # remove expected periods
+	line = line.replace("Mr.", "Mr")
+	line = line.replace("etc.", "etc")
+	line = line.replace("P.S.", "PS")
+	line = line.replace(".\"", "\".") # switch quote and punctuation to preserve quotes when splitting on punctuation
+	line = line.replace("?\"", "\"?")
+	line = line.replace("!\"", "\"!")
+	line = line.replace(".\'", "\'.")
+	line = line.replace("?\'", "\'?")
+	line = line.replace("!\'", "\'!")
+	line = line.replace("--", " ")
+
+	return line
+
+def get_sentences():
+	'''
+	get sentences from full texts
+	'''
+	raw_data_path = "%s/../data/raw_data" % os.getcwd()
+
+	author_text = {}
+
+	for file_name in os.listdir(raw_data_path):
+		print(file_name)
+		author = "_".join(file_name.split("__")[0].split())
+
+		# If we haven't seen this author, add an empty list to the dictionary
+		if author not in author_text:
+			author_text[author] = []
+
+		# Process the text of one of the books
+		full_text = open("%s/%s" % (raw_data_path, file_name), "r").read()
+		full_text = process_text(full_text)
+
+		# For each sentence, find each word, add start/end sentence tags, and add to full list of author text
+		for sentence in re.split("\. |\? |\! ", full_text):
+			arr = [" ".join(re.findall("[a-zA-Z]+'?[a-zA-Z]+", word)).lower() for word in sentence.split()]
+
+			sentence = " ".join(arr)
+			sentence = "<s> " + sentence + " </s>"
+			sentence = re.sub("\s+", " ", sentence) # replace any whitespace with a single space
+
+			author_text[author].append(sentence)
+
+	# Write out evey sentence for each author to their own file
+	for author in author_text:
+
+		with open("%s/../data/processed_data/%s.txt" % (os.getcwd(), author), "w") as ofile:
+			for sentence in author_text[author]:
+				ofile.write(sentence + "\n")
+
+if __name__ == "__main__":
+	get_sentences()
